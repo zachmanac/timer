705e980263444a4106a3425af38aa435fecfd9e0
zachmanac <zachstephenson94@gmail.com>
2022-11-14 15:16:02 -0800

    refactored function

--- timer1.js
+++ timer1.js
@@ -1,15 +1,15 @@
 const args = process.argv.slice(2);
 
 let timer = function(array) {
-  let timeout = 0;
+ 
 
   for (let arg of array) {
     let argN = Number(arg);
     if (argN >= 0 && !isNaN(argN)) {
       setTimeout(() => {
         process.stdout.write(`${argN}\x07\n`);
-      }, timeout = (argN * 1000));
-    } timeout;
+      }, argN * 1000);
+    }
   }
 };
 
